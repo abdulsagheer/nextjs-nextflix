8ccd7b39c3a8c89573c254a2bafa4e07ef302e38
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-06 21:48:46 +0530

    Added Banner, card and navbar components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Banner from "../components/Banner/Banner";
+import Card from "../components/Card/Card";
+import Navbar from "../components/Navbar/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Netflix App</h1>
+      <Navbar />
+      <Banner />
+      <Card />
     </div>
   );
 }
