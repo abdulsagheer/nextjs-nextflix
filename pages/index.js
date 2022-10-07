1d8081692e55dd39a0b8c92ff841a75873f399a0
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-07 22:47:12 +0530

    Added selection card component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,27 @@
+import Head from "next/head";
 import Banner from "../components/Banner/Banner";
-import Card from "../components/Card/Card";
+import SectionCard from "../components/Card/SelectionCard/SectionCard";
 import Navbar from "../components/Navbar/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Navbar username="abdulsagheeras29@gmail.com" />
       <Banner
         title="Clifford the red dog"
         subTitle="a very cute dog"
         imgUrl="/static/clifford.webp"
       />
-      <Card imgUrl="/static/clifford.webp" size="large" />
-      <Card imgUrl="/static/clifford.webp" size="medium" />
-      <Card imgUrl="/static/clifford.webp" size="small" />
+
+      <div className={styles.sectionWrapper}>
+        <SectionCard title="Disney" />
+      </div>
     </div>
   );
 }
