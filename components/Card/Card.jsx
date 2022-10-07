b9598fd73a11052a9af6e67b0d2feec2f9e34289
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-07 22:25:50 +0530

    Added transition motion

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,34 +1,46 @@
-import Image from "next/image";
 import { useState } from "react";
+import Image from "next/image";
+
+import cls from "classnames";
+import { motion } from "framer-motion";
+
 import styles from "./card.module.css";
 
-const Card = ({ imgUrl, size }) => {
+const Card = (props) => {
+  const {
+    imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
+    size = "medium",
+  } = props;
+
+  const [imgSrc, setImgSrc] = useState(imgUrl);
   const classMap = {
     large: styles.lgItem,
     medium: styles.mdItem,
     small: styles.smItem,
   };
 
-  const [imgSrc, setImgSrc] = useState(imgUrl);
-
-  const handleError = () => {
-    console.log("Hii Error");
+  const handleOnError = () => {
+    console.log("hii error");
     setImgSrc(
-      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1459&q=80"
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
     );
   };
 
   return (
     <div className={styles.container}>
-      <div className={classMap[size]}>
+      Card
+      <motion.div
+        className={cls(styles.imgMotionWrapper, classMap[size])}
+        whileHover={{ scale: 1.1 }}
+      >
         <Image
-          src={imgUrl}
+          src={imgSrc}
           alt="image"
           layout="fill"
-          onError={handleError}
+          onError={handleOnError}
           className={styles.cardImg}
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
