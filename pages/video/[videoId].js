208d7bafa90459f446e7626d02c57161bd491035
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-10 11:09:35 +0530

    Added Iframe for banner

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,10 +1,33 @@
-import React from "react";
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+import styles from "../../styles/Video.module.css";
 
-const VideoId = () => {
+Modal.setAppElement("#__next");
+
+const Video = () => {
   const router = useRouter();
   console.log({ router });
-  return <div>VideoId</div>;
+  return (
+    <div className={styles.container}>
+      <Modal
+        isOpen={true}
+        contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+      >
+        <iframe
+          id="ytplayer"
+          type="text/html"
+          width="100%"
+          height="360"
+          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+          frameBorder="0"
+        ></iframe>
+        <div>Modal body</div>
+      </Modal>
+    </div>
+  );
 };
 
-export default VideoId;
+export default Video;
